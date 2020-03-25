b7bf97f7c969dab5bddb38460dadb9599384b541
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-03-25 17:12:19 -0600

    all done, steping back

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {gsap} from 'gsap'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+
+  bee=()=>{
+      gsap.to('.box', { duration: 2,
+        ease: "bounce.out",
+        y: 200
+      })
+
+      gsap.to('.bean', { duration: 2,
+        ease: "bounce.out",
+        x: 200
+      })
+
+      gsap.to('.bart', { duration: 2,
+        ease: "bounce.out",
+        x: -200
+      })
+
+      gsap.to('.beep', { duration: 2,
+        ease: "bounce.out",
+        y: -200
+      })
+    }
+
+    boo=()=>{
+      gsap.to('.box', { duration: 2,
+        ease: "bounce.out",
+        y: 0
+      })
+
+      gsap.to('.bean', { duration: 2,
+        ease: "bounce.out",
+        x: 0
+      })
+
+      gsap.to('.bart', { duration: 2,
+        ease: "bounce.out",
+        x: 0
+      })
+
+      gsap.to('.beep', { duration: 2,
+        ease: "bounce.out",
+        y: 0
+      })
+    }
+
+  render(){
+    return (
+      <div className="App">
+        <button className='button1' onClick={()=>this.bee()} >1</button>
+
+        <button  className='button2' onClick={()=>this.boo()} >2</button>
+
+        <img className='box' src='https://dejpknyizje2n.cloudfront.net/marketplace/products/geometric-bee-logo-sticker-1561735738.2410054.png'/>
+
+        <img className='bean' src='https://dejpknyizje2n.cloudfront.net/marketplace/products/geometric-bee-logo-sticker-1561735738.2410054.png' />
+
+        <img className='bart' src='https://dejpknyizje2n.cloudfront.net/marketplace/products/geometric-bee-logo-sticker-1561735738.2410054.png' />
+
+        <img className='beep' src='https://dejpknyizje2n.cloudfront.net/marketplace/products/geometric-bee-logo-sticker-1561735738.2410054.png' />
+      </div>
+    )
+  }
 }
 
 export default App;
